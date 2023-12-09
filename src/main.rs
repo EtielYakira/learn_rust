68a7dd07d2fc5191b4a1be8e27274465422df05e
EtielYakira <etielyakira@gmail.com>
2023-12-09 23:42:25 +0200

    data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x  = 5;
+    println!("val is  {}", x);
+    x = 4;
+
+    println!("vale is {}", x);
+    
+    const DREAM_RUST: u32 = 10_000;
+    
+    println!("vale is {}", DREAM_RUST);
+
+
+    let tup = ("hi", 33, 44);
+    let (word, _num, _) = tup;
+    let _wordy = tup.1;
+
+    println!("{:?}",tup);    
+    println!("{}",word);    
+    //println!("{}",tup);    
+
 }
