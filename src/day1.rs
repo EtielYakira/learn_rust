8267a6da16de0a023b4eb889d62de19eeb6e42ee
EtielYakira <etielyakira@gmail.com>
2023-12-17 23:08:41 +0200

    clean day 1

--- src/day1.rs
+++ src/day1.rs
@@ -40,7 +40,6 @@ fn _get_large_measurements(measurements: Vec<i32>) -> i32 {
 }
 
 
-
 fn get_large_measurements_part_two(measurements: Vec<i32>) -> i32 {
     return measurements
     .iter()
@@ -54,47 +53,6 @@ fn get_large_measurements_part_two(measurements: Vec<i32>) -> i32 {
         if measurements[i..i+3].iter().sum::<i32>() < measurements[i+1..i+4].iter().sum() {
             return acc + 1;
         }
-
-        // let mut curr = 0; 
-        // let mut prev = 0;
-        
-        // let tetst = measurements[i..i+3];
-        // let tetstt = measurements[i+3..i+6];
-
-
-
-        // if i > 0 && i < 3
-        // {
-        //     curr = measurements[0..i].iter().sum();
-        //     prev = measurements[0..i-1].iter().sum();
-        //     // if  > measurements[i-1]{
-        //     //     return acc + 1;
-        //     // }
-        // }
-
-        // if i > 3  
-        // {
-        //     curr = measurements[0..i].iter().sum();
-        //     prev = measurements[0..i-1].iter().sum();
-        //     // measurements[0..i-1].iter().sum() < measurements[0..i].iter().sum();
-        // }
-
-        // if curr > prev 
-        // {
-        //    return acc + 1;
-        // }
-
-
-        // let curr_nums = [0, 3];
-        // let prev_num = [0,3];
-        // let mesurment_size = measurements.len();
-        // if i < 3 {
-        //     if i > 0 {
-        //         if measurements[i] > measurements[i-1]{
-        //             return acc + 1;
-        //         }
-        //     }
-        // }
         return acc;
     });
 }
