ea3c9c9f2db699b542ddb3e5383900c10baab73b
EtielYakira <etielyakira@gmail.com>
2024-01-11 23:49:59 +0200

    day 4 starting to work on bingo game

--- src/day4.rs
+++ src/day4.rs
@@ -1,3 +1,35 @@
 pub fn day4(){
+    let file_path = "";
+    let file_as_string = read_file_to_str(&file_path);
+        
+
+    let (boards, winning_numbers) = create_game_from_string(file_as_string);
     
+    
+
+
+
+
+}
+
+fn create_game_from_string(file_as_string: &str) -> (Vec<i32>,) {
+    todo!()
+}
+
+
+fn read_file_to_str(file_path: &str) -> &str {
+    todo!()
+}
+
+pub trait Game {
+    
+}
+
+pub struct Bingo {
+    pub table: Table 
+}
+
+pub struct Table{
+    pub rows: Vec<i32>,
+    pub colums: Vec<i32>
 }
