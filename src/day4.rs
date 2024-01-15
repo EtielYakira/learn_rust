d88f48b5c105e361fcc07e94c0d08113cc0ce9d2
EtielYakira <etielyakira@gmail.com>
2024-01-15 22:45:35 +0200

    raed file

--- src/day4.rs
+++ src/day4.rs
@@ -1,9 +1,16 @@
-pub fn day4(){
-    let file_path = "";
-    let file_as_string = read_file_to_str(&file_path);
-        
+use std::{path::Path, fs};
+use itertools::Itertools;
 
-    let (boards, winning_numbers) = create_game_from_string(file_as_string);
+
+pub fn day4(){
+    let file_path = Path::new("./src/DATA/day4.txt");
+    let content = fs::read_to_string(file_path)
+        .expect("need to read file");
+
+
+
+
+    let (boards, winning_numbers) = create_game_from_string(content);
     
     
 
@@ -12,7 +19,19 @@ pub fn day4(){
 
 }
 
-fn create_game_from_string(file_as_string: &str) -> (Vec<i32>,) {
+fn create_game_from_string(file_as_string: String) -> (Vec<i32>,Vec<i32>) {
+    
+    let data: Vec<&str> = file_as_string 
+    .lines()
+    .map(|x| x)
+    .collect();
+
+
+    let line_winning_numbers: Vec<i32> = data[0].split(',').map(|s| s.parse().unwrap()).collect();
+
+    let games_str = &data[1..];    
+
+    let games = games_str.iter().filter(|x| x.is_empty()).chunks(5);
     todo!()
 }
 
